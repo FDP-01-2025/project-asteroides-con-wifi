59397f687a0fc79f2c2ed7f677cfa9d99436b869
Alvaro Campos <alvarocampos1563@gmail.com>
2025-06-05 10:09:15 -0600

    New Files :memo:

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"**************************************************"<<endl;
+    cout<<"************Bienvenido a OWL ROCKET***************"<<endl;
+    cout<<"**************************************************"<<endl;
+    cout<<""<<endl;
+    cout<<"Tu objetivo en este juego es ayudar a Noctriz a rescatar a su planeta"<<endl;
+
+    cout<<""<<endl;
+
+
+    cout << "      ^      \n";
+    cout << "     / \\     \n";
+    cout << "    /___\\    \n";
+    cout << "    |   |    \n";
+    cout << "    |   |    \n";
+    cout << "    |___|    \n";
+    cout << "   /|   |\\   \n";
+    cout << "  /_|___|_\\  \n";
+    cout << "    / | \\    \n";
+    cout << "   *  |  *   \n";
+    cout << "  *   |   *  \n";
+    cout << " *    |    * \n";
+    cout << "*     |     *\n";
+    
+    return 0;
+}
---
+++ utilities.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+void welcome(){
+    cout << "========================================\n";
+    cout << "   ¡Bienvenido comandante de Algoritmia!\n";
+    cout << "   Ayuda a Noctriz a salvar su planeta\n";
+    cout << "========================================\n";
+}
+    return 0;
+}
