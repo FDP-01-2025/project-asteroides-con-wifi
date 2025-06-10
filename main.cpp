6767e6c2e85b8bfab68bc33780da518d68a5bda1
Alvaro Campos <alvarocampos1563@gmail.com>
2025-06-10 10:29:31 -0600

    Update :New File:

--- main.cpp
+++ main.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
+#include <cmath>
+#include <utilities.h>
 
 using namespace std;
 
 int main(){
-
-    cout<<"**************************************************"<<endl;
-    cout<<"************Bienvenido a OWL ROCKET***************"<<endl;
-    cout<<"**************************************************"<<endl;
-    cout<<""<<endl;
-    cout<<"Tu objetivo en este juego es ayudar a Noctriz a rescatar a su planeta"<<endl;
-
-    cout<<""<<endl;
-
-
-    cout << "      ^      \n";
-    cout << "     / \\     \n";
-    cout << "    /___\\    \n";
-    cout << "    |   |    \n";
-    cout << "    |   |    \n";
-    cout << "    |___|    \n";
-    cout << "   /|   |\\   \n";
-    cout << "  /_|___|_\\  \n";
-    cout << "    / | \\    \n";
-    cout << "   *  |  *   \n";
-    cout << "  *   |   *  \n";
-    cout << " *    |    * \n";
-    cout << "*     |     *\n";
+   
+    welcome();
     
     return 0;
 }
--- utilities.h
+++ utilities.h
@@ -1,5 +1,44 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#include <iostream>
+#include <cstdlib>
 
-#endif
+using namespace std;
+
+// This function can clear the screen
+inline void clrscr() {
+    system("cls"); 
+}
+
+// This function print a menu
+inline void welcome() {
+    cout << "========================================" << endl;
+    cout << "   ¡Welcome Algorithm Commander!        " << endl;
+    cout << "   Help Noctriz rescue his planet       " << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+    cout << "Press any key to continue..." << endl;
+    clrscr();
+}
+
+// Esta función imprime el menú
+inline void menu() {
+
+    cout << endl;
+    cout << "      ^     " << endl;
+    cout << "     / \\     " << endl;
+    cout << "    /___\\    " << endl;
+    cout << "    |   |    " << endl;
+    cout << "    |   |    " << endl;
+    cout << "    |___|    " << endl;
+    cout << "   /|   |\\   " << endl;
+    cout << "  /_|___|_\\  " << endl;
+    cout << "    / | \\    " << endl;
+    cout << "   *  |  *   " << endl;
+    cout << "  *   |   *  " << endl;
+    cout << " *    |    * " << endl;
+    cout << "*     |     *" << endl;
+}
+
+#endif // UTILITIES_H
