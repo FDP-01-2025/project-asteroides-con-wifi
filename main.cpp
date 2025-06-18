d8d7400cd2a9e577688447394823128792d02901
rubenUCA <00076325@uca.edu.sv>
2025-06-18 15:44:47 -0600

    Updated Main :art:

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
-#include <utilities.h>
-#include <Lib/memorama.h>
+#include "utilities.h"
+#include "Lib/memorama.h"
 
 using namespace std;
 
 int main(){
-   
-    welcome();
+    Memorama juego;
+    juego.playGame();
     
     return 0;
 }
