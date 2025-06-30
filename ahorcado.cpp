bc2658e5ff8efd803a4a8c98d803c66e7be73960
Keiryn21 <00061225@uca.edu.sv>
2025-06-30 16:46:21 -0600

    ahorcado

---
+++ ahorcado.cpp
@@ -0,0 +1,17 @@
+//incluir librerias//
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+//declarar las funciones//
+
+void Cleanscreen();
+void Playgame();
+void body(int life);
+
+//Palabras que aparecen en el juego//
+
+
+
+
