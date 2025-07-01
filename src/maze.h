94d7b1d5948d5a1243ed9cb777924898e155b523
Leila Saraí Campos Peraza <00141525@uca.edu.sv>
2025-07-01 17:28:50 -0600

    update

---
+++ src/maze.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+char getch() {
+    char buf = 0; 
+    struct termios old = {};
+    if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+    old.c
+    _lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0) perror("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("tcsetattr ~ICANON");
+    return buf;
+}
+#endif
+
+using namespace std;
+
+const int lines = 10;
+const int columns = 20;
+const char WALL = '|';
+const char PATH = ' ';
+const char PLAYER = '@';
+const char ENTRY = 'E';
+const char EXIT = 'S';
+}
