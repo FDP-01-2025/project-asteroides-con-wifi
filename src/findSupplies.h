346924ecfaf6b2ea81c575a044446116118ad447
Alvaro Campos <alvarocampos1563@gmail.com>
2025-07-07 10:10:42 -0600

    Update :wrench:

---
+++ src/findSupplies.h
@@ -0,0 +1,9 @@
+#ifndef FIND_SUPPLIES_H
+#define FIND_SUPPLIES_H
+
+// Declaraciones de funciones globales para findSupplies.cpp
+void printMaze();
+bool inside(int r, int c);
+void findSupplies();
+
+#endif // FIND_SUPPLIES_H
