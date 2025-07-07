40006862c90aeef8499b7137adbdad4702c1d23a
Alvaro Campos <alvarocampos1563@gmail.com>
2025-07-07 10:13:48 -0600

    Update :wrench:

---
+++ src/searchInfo.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_INFO_H
+#define SEARCH_INFO_H
+
+// Declaraciones de funciones globales para searchInfo.cpp
+void initializeGame();
+void showBoard();
+void play();
+
+#endif // SEARCH_INFO_H
