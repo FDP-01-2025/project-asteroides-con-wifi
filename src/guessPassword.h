14629f2f0ae7ce6696b616ddeb3bbbd0ca2905ba
Alvaro Campos <alvarocampos1563@gmail.com>
2025-07-07 10:11:52 -0600

    Update :wrench:

---
+++ src/guessPassword.h
@@ -0,0 +1,9 @@
+#ifndef GUESS_PASSWORD_H
+#define GUESS_PASSWORD_H
+
+// Declaraciones de funciones globales para guessPassword.cpp
+void body(int life);
+void showProgress();
+void guessPassword(); // Asegúrate de que esta línea esté presente y correcta
+
+#endif // GUESS_PASSWORD_H
